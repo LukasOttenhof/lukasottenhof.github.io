8c508dfdc72e43e3de8521673a9481656dde1a06
Lukas . <lukas.@Lukass-MacBook-Air.local>
2023-09-12 22:19:23 -0600

    h

--- TarynArt/script.js
+++ TarynArt/script.js
@@ -1,8 +1,25 @@
-document.getElementById("mobile-menu-button").addEventListener("click", function () {
-    var mobileMenu = document.getElementById("mobile-menu");
-    if (mobileMenu.style.display === "block" || mobileMenu.style.display === "") {
-        mobileMenu.style.display = "none";
-    } else {
-        mobileMenu.style.display = "block";
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuButton = document.getElementById("mobile-menu-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    mobileMenuButton.addEventListener("click", function () {
+        mobileMenu.classList.toggle("active");
+    });
+
+    // Add event listener to close the menu when clicking outside of it
+    document.addEventListener("click", function (event) {
+        if (!mobileMenu.contains(event.target) && event.target !== mobileMenuButton) {
+            mobileMenu.classList.remove("active");
+        }
+    });
+
+    // Select all links inside the mobile menu
+    const mobileMenuLinks = mobileMenu.querySelectorAll("a.menu-link");
+
+    // Add event listeners to close the menu when clicking a link
+    mobileMenuLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            mobileMenu.classList.remove("active");
+        });
+    });
 });
